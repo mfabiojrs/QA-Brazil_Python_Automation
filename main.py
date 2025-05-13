72aa5f2c0fde7b89dc06f7aa3ff37dc754547a08
Fabio <m.fabiojr.s@gmail.com>
2025-05-13 20:33:49 -0300

    preparing main.py
    functions created, parameters passed, body defined

--- main.py
+++ main.py
@@ -1,2 +1,50 @@
+import data, helpers # <- importando
 
 class TestUrbanRoutes:
+
+    #Definindo função
+    # - Use a palavra-chave 'def'
+    # - Escreva o nome da função
+    # - Coloque os parâmetros entre parênteses ()
+    # - Termine a linha com dois-pontos :
+    # - Escreva o corpo da função indentado (com recuo)
+
+    def test_set_route(self):
+        #Adicionar em S8
+        print("função criada para definir a rota")  # Exibe a mensagem especificada na tela.
+        pass  # 'pass' indica que a função ainda não faz nada
+
+    def test_select_plan(self):
+        #Adicionar em S8
+        print("função criada para selecionar o plano")  # Exibe a mensagem especificada na tela.
+        pass  # 'pass' indica que a função ainda não faz nada
+
+    def test_fill_phone_number(self):
+        #Adicionar em S8
+        print("função criada para preencher o número de telefone")  # Exibe a mensagem especificada na tela.
+        pass  # 'pass' indica que a função ainda não faz nada
+
+    def test_fill_card(self):
+        #Adicionar em S8
+        print("função criada para preencher o cartão")  # Exibe a mensagem especificada na tela.
+        pass  # 'pass' indica que a função ainda não faz nada
+
+    def test_comment_for_driver(self):
+        #Adicionar em S8
+        print("função criada para definir comentário ao motorista")  # Exibe a mensagem especificada na tela.
+        pass  # 'pass' indica que a função ainda não faz nada
+
+    def test_order_blanket_and_handkerchiefs(self):
+        #Adicionar em S8
+        print("função criada para definir cobertor e lenços")  # Exibe a mensagem especificada na tela.
+        pass  # 'pass' indica que a função ainda não faz nada
+
+    def test_order_2_ice_creams(self):
+        #Adicionar em S8
+        print("função criada para selecionar 2 sorvetes")  # Exibe a mensagem especificada na tela.
+        pass  # 'pass' indica que a função ainda não faz nada
+
+    def test_car_search_model_appears(self):
+        #Adicionar em S8
+        print("função criada para definir o modelo de busca de carro")  # Exibe a mensagem especificada na tela.
+        pass  # 'pass' indica que a função ainda não faz nada
