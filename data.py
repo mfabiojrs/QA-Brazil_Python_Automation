64454f869950ab911673b53df339db8cd9d3b520
Fabio <m.fabiojr.s@gmail.com>
2025-05-13 19:21:42 -0300

    add data.py
    constant definition files with descriptive inline comments

--- data.py
+++ data.py
@@ -1,1 +1,12 @@
 # Arquivo com constantes
+
+# URL do servidor de teste
+URBAN_ROUTES_URL    = ''
+
+# Descrição             Dados de teste
+ADDRESS_FROM        = 'East 2nd Street, 601'    # Endereço do local da partida
+ADDRESS_TO          = '1300 1st St'             # Destino da corrida
+PHONE_NUMBER        = '+1 123 123 12 12'        # Número de telefone do cliente
+CARD_NUMBER         = '1234 5678 9100'          # Número do cartão de crédito do cliente
+CARD_CODE           = '1111'                    # Código do cartão de crédito do cliente
+MESSAGE_FOR_DRIVER  = 'Pare no bar de sucos'    # Mensagem para o motorista
