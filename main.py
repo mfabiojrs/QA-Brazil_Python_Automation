2d6c93653610a04e735ef769a836ca4fa9abab84
Fabio <m.fabiojr.s@gmail.com>
2025-05-26 23:44:52 -0300

    Add 2s sleep at the end of each test

--- main.py
+++ main.py
@@ -31,29 +31,35 @@ class TestUrbanRoutes:
         self.driver.get(data.URBAN_ROUTES_URL)
         urban_routes_page = UrbanRoutesPage(self.driver)
         urban_routes_page.enter_locations(data.ADDRESS_FROM, data.ADDRESS_TO)
+        time.sleep(2) # Apenas para conseguir visualizar o teste sem que o browser feche muito rápido após concluir o teste
 
     def test_select_plan(self):
         self.driver.get(data.URBAN_ROUTES_URL)
         urban_routes_page = UrbanRoutesPage(self.driver)
         urban_routes_page.choose_comfort_car(data.ADDRESS_FROM, data.ADDRESS_TO)
+        time.sleep(2) # Apenas para conseguir visualizar o teste sem que o browser feche muito rápido após concluir o teste
+
 
     def test_fill_phone_number(self):
         self.driver.get(data.URBAN_ROUTES_URL)
         urban_routes_page = UrbanRoutesPage(self.driver)
         urban_routes_page.choose_comfort_car(data.ADDRESS_FROM, data.ADDRESS_TO)
         urban_routes_page.add_phone_number(data.PHONE_NUMBER)
+        time.sleep(2) # Apenas para conseguir visualizar o teste sem que o browser feche muito rápido após concluir o teste
 
     def test_fill_card(self):
         self.driver.get(data.URBAN_ROUTES_URL)
         urban_routes_page = UrbanRoutesPage(self.driver)
         urban_routes_page.choose_comfort_car(data.ADDRESS_FROM, data.ADDRESS_TO)
         urban_routes_page.choose_card_as_payment_method(data.CARD_NUMBER, data.CARD_CODE)
+        time.sleep(2) # Apenas para conseguir visualizar o teste sem que o browser feche muito rápido após concluir o teste
 
     def test_comment_for_driver(self):
         self.driver.get(data.URBAN_ROUTES_URL)
         urban_routes_page = UrbanRoutesPage(self.driver)
         urban_routes_page.choose_comfort_car(data.ADDRESS_FROM, data.ADDRESS_TO)
         urban_routes_page.enter_message_to_driver(data.MESSAGE_FOR_DRIVER)
+        time.sleep(2) # Apenas para conseguir visualizar o teste sem que o browser feche muito rápido após concluir o teste
 
     def test_order_blanket_and_handkerchiefs(self):
         self.driver.get(data.URBAN_ROUTES_URL)
@@ -61,12 +67,14 @@ class TestUrbanRoutes:
         urban_routes_page.choose_comfort_car(data.ADDRESS_FROM, data.ADDRESS_TO)
         urban_routes_page.click_button_blanket_and_sheets()
         assert urban_routes_page.is_blanket_and_sheets_selected(), f"O interruptor do cobertor e lenços não foi alterado."
+        time.sleep(2) # Apenas para conseguir visualizar o teste sem que o browser feche muito rápido após concluir o teste
 
     def test_order_2_ice_creams(self):
         self.driver.get(data.URBAN_ROUTES_URL)
         urban_routes_page = UrbanRoutesPage(self.driver)
         urban_routes_page.choose_comfort_car(data.ADDRESS_FROM, data.ADDRESS_TO)
         urban_routes_page.click_button_ice_cream()
+        time.sleep(2) # Apenas para conseguir visualizar o teste sem que o browser feche muito rápido após concluir o teste
 
     def test_car_search_model_appears(self):
         self.driver.get(data.URBAN_ROUTES_URL)
@@ -74,6 +82,7 @@ class TestUrbanRoutes:
         urban_routes_page.choose_comfort_car(data.ADDRESS_FROM, data.ADDRESS_TO)
         urban_routes_page.enter_message_to_driver(data.MESSAGE_FOR_DRIVER)
         urban_routes_page.click_button_car_search()
+        time.sleep(2) # Apenas para conseguir visualizar o teste sem que o browser feche muito rápido após concluir o teste
 
     @classmethod
     def teardown_class(cls):
